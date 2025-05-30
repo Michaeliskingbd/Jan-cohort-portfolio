588153e075668aeb2955adef3a67c027e17d87a6
Michael Aladejuyigbe <michaeliskingbd@gmail.com>
2025-05-30 22:21:30 +0100

    first commit

---
+++ src/component/Herosection.jsx
@@ -0,0 +1,48 @@
+const Herosection = () => {
+  return (
+    <section className="relative h-screen bg-cover bg-center bg-[url('assets/background.jpg')] text-white">
+      <div className=" z-0 absolute inset-0 bg-black bg-opacity-70"></div>
+
+      <nav className="absolute top-0 z-20 flex w-full justify-between items-center px-10 py-8">
+        <div>
+          <h1>LOGO</h1>
+        </div>
+        <div>
+          <ul className="flex text-lg font-semibold gap-9 transition-all duration-200">
+            <li className="text-gray-400 hover:text-white">About</li>
+            <li className="text-gray-400 hover:text-white">Portfolio</li>
+            <li className="text-gray-400 hover:text-white">Services</li>
+            <li className="text-gray-400 hover:text-white">Testimonials</li>
+          </ul>
+        </div>
+        <div>
+          <button className="ring-1 ring-white px-8 py-3 font-semibold hover:bg-white hover:text-blue-700 transition-all duration-200">
+            Let's connect
+          </button>
+        </div>
+      </nav>
+
+      <section className="relative z-10 text-white flex flex-col items-center justify-center h-full">
+        <div className="text-center w-[60%]">
+          <h1 className="text-6xl font-bold leading-snug">
+            Freelance web developer based in Lagos
+          </h1>
+          <p className="text-lg mt-4">
+            Expert development services by NYC-based freelancer for your website
+            needs.
+          </p>
+          <div className="mt-10 space-x-3">
+            <button className="px-10 py-4 bg-purple-600 hover:text-purple-600 hover:bg-white font-semibold">
+              Let's connect
+            </button>
+            <button className="ring-1 px-10 py-4 font-semibold hover:text-purple-600 hover:bg-white">
+              Check my work
+            </button>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Herosection;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Herosection from "../component/Herosection";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Herosection />
+    </div>
+  );
+};
+
+export default Homepage;
